3266e09866922b28adb1ef9864cacde56e34ca0a
Leonardo Figorelli Santana <leofigorelli@gmail.com>
2025-09-22 21:20:32 -0300

    first commit

---
+++ src/main/java/dev/lest/GymPlanningAi/GymPlanningAiApplication.java
@@ -0,0 +1,13 @@
+package dev.lest.GymPlanningAi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymPlanningAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymPlanningAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/lest/GymPlanningAi/GymPlanningAiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.lest.GymPlanningAi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymPlanningAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
